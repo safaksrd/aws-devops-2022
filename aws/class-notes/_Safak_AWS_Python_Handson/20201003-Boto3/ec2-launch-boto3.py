26176aaf6aa525c31018e632789fdf1e87ba4121
safaksrd <safaksrd@gmail.com>
2022-01-22 21:59:59 +0100

    20201003-Boto3 wurde nachgeholt

--- aws/class-notes/_Safak_AWS_Python_Handson/20201003-Boto3/ec2-launch-boto3.py
+++ aws/class-notes/_Safak_AWS_Python_Handson/20201003-Boto3/ec2-launch-boto3.py
@@ -3,9 +3,9 @@ ec2 = boto3.resource('ec2')
 
 # create a new EC2 instance
 instances = ec2.create_instances(
-     ImageId='ami-0dba2cb6798deb6d8',
+     ImageId='ami-08e4e35cccc6189f4',
      MinCount=1,
      MaxCount=1,
      InstanceType='t2.micro',
-     KeyName='guilesKp'
+     KeyName='xxxx' # ec2 instance olusturmak icin key i girmelisin
  )
--- aws/class-notes/_Safak_AWS_Python_Handson/20201003-Boto3/s3-put-boto3.py
+++ aws/class-notes/_Safak_AWS_Python_Handson/20201003-Boto3/s3-put-boto3.py
@@ -5,4 +5,5 @@ s3 = boto3.resource('s3')
 
 # Upload a new file
 data = open('test.jpg', 'rb')
-s3.Bucket('guiles').put_object(Key='test.jpg', Body=data)
+s3.Bucket('bucketadigirilmeli').put_object(Key='test.jpg', Body=data)
+# test.jpg python dosyasi ile ayni dizinde olmali
