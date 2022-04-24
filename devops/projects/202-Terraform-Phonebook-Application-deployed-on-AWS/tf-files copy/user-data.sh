9c5b76d41d81c3f160873765f5fbc9274ac75d17
safaksrd <safaksrd@gmail.com>
2022-04-24 20:16:12 +0200

    202-Terraform-Phonebook-Application-deployed-on-AWS new changes

--- devops/projects/202-Terraform-Phonebook-Application-deployed-on-AWS/tf-files copy/user-data.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/bash
-yum update -y
-yum install python3 -y
-pip3 install flask
-pip3 install flask_mysql
-yum install git -y
-TOKEN="xxx"
-cd /home/ec2-user && git clone https://$TOKEN@github.com/safaksrd/phonebook-tf.git
-python3 /home/ec2-user/phonebook-tf/phonebook-app.py
